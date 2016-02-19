3b4f87477d5ae588d92cb25551b9400eb69d7646
Andrea <happtur@users.noreply.github.com>
2016-02-19 02:53:26 +0200

    JavaDoc for methods

--- minesweeper/src/main/java/ohha/minesweeper/logic/Game.java
+++ minesweeper/src/main/java/ohha/minesweeper/logic/Game.java
@@ -10,7 +10,7 @@ public class Game {
     private Grid grid;
     private int turnedTiles;
 
-    Game(Grid grid) {
+    public Game(Grid grid) {
         this.turnedTiles = 0;
         this.grid = grid;
     }
@@ -20,13 +20,41 @@ public class Game {
         this.grid = new Grid(size, bombs);
     }
 
+    /**
+     * The method returns the size of the grid's axes. So if the grid's size
+     * is 4*4, this method returns 4.
+     *
+     * @return size of grid's axes
+     */
+    
     public int getSizeOfGrid() {
         return this.grid.getSize();
     }
+    
+    /**
+     * The method returns true if the tile with the coordinate-values that
+     * correspond to the parameters is turned.
+     *
+     * @param x the x-coordinate of the tile
+     * @param y the y-coordinate of the tile
+     * 
+     * @return boolean value for "the tile is turned"
+     */
 
     public boolean isTurned(int x, int y) {
         return this.grid.getTile(x, y).isTurned();
     }
+    
+    /**
+     * The method turns the string given as a parameter into an int-array with
+     * two elements, where the first element is the x-coordinate and the
+     * second the y-coordinate.
+     *
+     * @param xy string in the format "1:2", where 1 is the x-coordinate and
+     * 2 is the y-coordinate
+     * 
+     * @return array with two integers representing coordinates
+     */
     
     public int[] toCoordinates(String xy) {
         String[] coordinates = xy.split(":");
@@ -36,28 +64,33 @@ public class Game {
         int[] coorInt = new int[]{x,y};
         return coorInt;
     }
-
-//    public int getValue(String xy) {
-//        String[] coordinates = xy.split(":");
-//        int x = Integer.parseInt(coordinates[0]);
-//        int y = Integer.parseInt(coordinates[1]);
-//        
-//        return getValue(x, y);
-//    }
+    
+    /**
+     * The method returns the value of the tile with the coordinates that
+     * correspond to the parameters.
+     *
+     * @param x the x-coordinate of the tile
+     * @param y the y-coordinate of the tile
+     * 
+     * @return the value of the tile
+     */
     
     public int getValue(int x, int y) {
         return this.grid.getTile(x, y).getValue();
     }
+
+    /**
+     * The method tries to turn the tile with the specific coordinates given as
+     * parameters. If the tile is a bomb or the last non-flagged, non-bomb the
+     * method returns false, i.e. the game is over. Otherwise the game continues
+     * and the method returns true
+     *
+     * @param x the x-coordinate of the tile
+     * @param y the y-coordinate of the tile
+     * 
+     * @return boolean value for "the game continues"
+     */
     
-
-//    public boolean turn(String xy) {
-//        String[] coordinates = xy.split(":");
-//        int x = Integer.parseInt(coordinates[0]);
-//        int y = Integer.parseInt(coordinates[1]);
-//        
-//        return this.turn(x, y);
-//    }
-
     public boolean turn(int x, int y) {
         if (!this.grid.tileOnGrid(x, y)) {
             return true;
@@ -74,13 +107,14 @@ public class Game {
         return true;
     }
     
-//    public void flag(String xy) {
-//        String[] coordinates = xy.split(":");
-//        int x = Integer.parseInt(coordinates[0]);
-//        int y = Integer.parseInt(coordinates[1]);
-//        
-//        this.flag(x, y);
-//    }
+    /**
+     * The method calls the flag-method of the tile with the coordinates
+     * corresponding to the parameters.
+     *
+     * @param x the x-coordinate of the tile
+     * @param y the y-coordinate of the tile
+     * 
+     */
 
     public void flag(int x, int y) {
         if (!this.grid.tileOnGrid(x, y)) {
@@ -88,19 +122,27 @@ public class Game {
         }
         this.grid.getTile(x, y).flag();
     }
+    
+    /**
+     * The method returns true if the game is over and the player won.
+     * 
+     * @return boolean value for "the game is won"
+     */
 
     public boolean isWon() {
         int size = this.grid.getSize();
         return this.turnedTiles == size * size - this.grid.amountOfBombs();
     }
-
-//    public boolean isFlagged(String xy) {
-//        String[] coordinates = xy.split(":");
-//        int x = Integer.parseInt(coordinates[0]);
-//        int y = Integer.parseInt(coordinates[1]);
-//        
-//        return this.isFlagged(x, y);
-//    }
+    
+    /**
+     * The method returns true if the tile with the coordinates that
+     * correspond to the parameters is flagged.
+     *
+     * @param x the x-coordinate of the tile
+     * @param y the y-coordinate of the tile
+     * 
+     * @return boolean value for "the tile is flagged"
+     */
     
     public boolean isFlagged(int x, int y) {
         return this.grid.getTile(x, y).isFlagged();
